6d29aa84ba2c83c26909fa29dc2e9fda4b0741ef
kevenallan <kevenallan98@gmail.com>
2024-09-27 21:38:41 -0300

    Ajustando o spring security

--- src/main/java/br/com/sharebox/security/SecurityConfig.java
+++ src/main/java/br/com/sharebox/security/SecurityConfig.java
@@ -1,13 +1,119 @@
 package br.com.sharebox.security;
 
+//import java.util.List;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    private final TokenInterceptorFilter tokenInterceptorFilter;
+//
+//    public SecurityConfig(TokenInterceptorFilter headerPrintFilter) {
+//        this.tokenInterceptorFilter = headerPrintFilter;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//            .csrf(csrf -> csrf.disable())
+//            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//            .cors(cors -> cors.configurationSource(request -> {
+//                var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+//                corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200")); // Permita a origem desejada
+//                corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//                corsConfiguration.setAllowedHeaders(List.of("*"));
+//                corsConfiguration.setAllowCredentials(true);
+//                return corsConfiguration;
+//            }))
+//            .authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/usuario/login", "/register", "/public/**").permitAll() // Permite acesso sem autenticação
+//                .requestMatchers("/arquivo/listar").authenticated() // Permite acesso a /arquivo/listar somente para usuários autenticados
+//                .anyRequest().authenticated()) // Requer autenticação para qualquer outra requisição
+//            .addFilterBefore(tokenInterceptorFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
+//}
+
+//import java.util.Arrays;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//    private final TokenInterceptorFilter tokenInterceptorFilter;
+//    public SecurityConfig(TokenInterceptorFilter tokenInterceptorFilter) {
+//        this.tokenInterceptorFilter = tokenInterceptorFilter;
+//    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//            .csrf(csrf -> csrf.disable())
+////            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//            .authorizeHttpRequests(authorize -> 
+//                authorize
+//                    .requestMatchers("/usuario/login", "/cadastrar").permitAll()  // Permitir login e registro
+//                    .anyRequest().authenticated()  // Todas as outras requisições precisam de autenticação
+//            );
+//        http.addFilterBefore(tokenInterceptorFilter, UsernamePasswordAuthenticationFilter.class);
+//        return http.build();
+//    }
+//    
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true); // Permitir credenciais, se necessário
+////        config.addAllowedOrigin("http://192.168.0.5:4200"); // Origem permitida
+//        config.addAllowedOrigin("http://localhost:4200"); // Origem permitida
+//        config.addAllowedHeader("*"); // Todos os cabeçalhos são permitidos
+//        config.addAllowedMethod("GET"); // Permitir GET
+//        config.addAllowedMethod("POST"); // Permitir POST
+//        config.addAllowedMethod("PUT"); // Permitir PUT
+//        config.addAllowedMethod("DELETE"); // Permitir DELETE
+//        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+//        config.addAllowedMethod("OPTIONS"); // Permitir OPTIONS
+//        source.registerCorsConfiguration("/**", config); // Aplicar para todos os endpoints
+//        return new CorsFilter(source);
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+//}
+
+
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +121,8 @@ public class SecurityConfig {
 
     private final TokenInterceptorFilter tokenInterceptorFilter;
 
-    public SecurityConfig(TokenInterceptorFilter headerPrintFilter) {
-        this.tokenInterceptorFilter = headerPrintFilter;
+    public SecurityConfig(TokenInterceptorFilter tokenInterceptorFilter) {
+        this.tokenInterceptorFilter = tokenInterceptorFilter;
     }
 
     @Bean
@@ -24,20 +130,42 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .cors(cors -> cors.configurationSource(request -> {
-                var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+                var corsConfiguration = new CorsConfiguration();
                 corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200")); // Permita a origem desejada
                 corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 corsConfiguration.setAllowedHeaders(List.of("*"));
                 corsConfiguration.setAllowCredentials(true);
                 return corsConfiguration;
             }))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/usuario/login", "/register", "/public/**").permitAll()
-                .anyRequest().authenticated())
-            .addFilterBefore(tokenInterceptorFilter, UsernamePasswordAuthenticationFilter.class);
+            .authorizeHttpRequests(auth -> 
+                auth
+                    .requestMatchers("/usuario/login", "/usuario/cadastrar").permitAll() // Permite acesso sem autenticação
+                    .anyRequest().authenticated() // Requer autenticação para qualquer outra requisição
+            )
+            .addFilterBefore(tokenInterceptorFilter, UsernamePasswordAuthenticationFilter.class); // Adiciona o filtro de token
 
         return http.build();
     }
-    
-    
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager(); // Fornece um gerenciador de autenticação
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200"); // Origem permitida
+        config.addAllowedHeader("*"); // Todos os cabeçalhos são permitidos
+        config.addAllowedMethod("GET"); // Permitir GET
+        config.addAllowedMethod("POST"); // Permitir POST
+        config.addAllowedMethod("PUT"); // Permitir PUT
+        config.addAllowedMethod("DELETE"); // Permitir DELETE
+        config.addAllowedMethod("OPTIONS"); // Permitir OPTIONS
+        source.registerCorsConfiguration("/**", config); // Aplicar para todos os endpoints
+        return new CorsFilter(source);
+    }
 }
+
--- src/main/java/br/com/sharebox/security/TokenInterceptorFilter.java
+++ src/main/java/br/com/sharebox/security/TokenInterceptorFilter.java
@@ -1,27 +1,63 @@
 package br.com.sharebox.security;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+
+import br.com.sharebox.service.AuthService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 @Component
 public class TokenInterceptorFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private AuthService authService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // Captura o valor do header "Authorization" (ou qualquer outro)
-        String headerValue = request.getHeader("Authorization");
-        
-        // Printar o valor do header no console
-        System.out.println("Header Authorization: " + headerValue);
+        // Captura o valor do header "Authorization"
+        String authorizationHeader = request.getHeader("Authorization");
+        String uri = request.getRequestURI();
 
-        // Continue com o restante da cadeia de filtros
+        // Printar o valor do header no console
+        System.out.println("Header Authorization: " + authorizationHeader);
+        System.out.println("URI: " + uri);
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7); // Remove "Bearer " do token
+            try {
+                this.authService.validateToken(token);
+                String userId = this.authService.extractUserId(token);
+                this.authService.uuidUsuarioLogado = userId;
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userId, null, null);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (JWTDecodeException e) {
+                System.out.println("Invalid JWT token: " + e.getMessage());
+                SecurityContextHolder.clearContext();
+            } catch (TokenExpiredException e) {
+            	System.out.println("TOKEN EXPIRADO: " + e.getMessage());
+                SecurityContextHolder.clearContext();
+            } catch (Exception e) {
+                System.out.println("Token Interceptor Error: " + e.getMessage());
+                SecurityContextHolder.clearContext();
+            }
+        }
+
+        // Continua com a cadeia de filtros
         filterChain.doFilter(request, response);
     }
 }
